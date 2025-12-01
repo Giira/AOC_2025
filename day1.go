75a581ee0b77fcd1ce255efb9a2f1c47ccdae97a
Giira <euan.a.marsh@gmail.com>
2025-12-01 17:25:37 +0000

    D1 P2 solve

--- day1.go
+++ day1.go
@@ -25,17 +25,26 @@ func day1() {
 
 	dial := 50
 	counter := 0
+	counter_2 := 0
 	for _, instruction := range data {
 		instruction = strings.TrimSpace(instruction)
 		direction := instruction[0]
 		distance_str := instruction[1:]
+		add_turns_str := "0"
 		if len(distance_str) != 2 && len(distance_str) != 1 {
+			add_turns_str = string(distance_str[0])
 			distance_str = distance_str[1:]
 		}
 		distance, err := strconv.Atoi(distance_str)
 		if err != nil {
 			log.Fatalf("error: Failed to convert distance string to integer: %v", err)
 		}
+		add_turns, err := strconv.Atoi(add_turns_str)
+		if err != nil {
+			log.Fatalf("error: Failed to convert distance string to integer: %v", err)
+		}
+
+		fmt.Printf("Dial at: %v, Turns %v, By %v\n", dial, string(direction), distance)
 
 		switch direction {
 		case 'R':
@@ -46,13 +55,26 @@ func day1() {
 			log.Printf("Direction not specified: %v", instruction)
 		}
 		if dial < 0 {
+			if dial+distance != 0 {
+				counter_2 += 1
+			}
 			dial += 100
+			fmt.Println("Dial passed 0")
 		} else if dial > 99 {
 			dial -= 100
+			if dial != 0 {
+				counter_2 += 1
+			}
+			fmt.Println("Dial passed 0")
 		}
-		if dial == 0 {
+		if dial == 0 && distance != 0 {
 			counter += 1
+			counter_2 += 1
+			fmt.Println("Dial at 0")
 		}
+		counter_2 += add_turns
+		fmt.Println(counter_2)
 	}
 	fmt.Printf("Day 1, Part 1: %v\n", counter)
+	fmt.Printf("Day 1, Part 2: %v\n", counter_2)
 }
