6999511776449677003cf55358671ab9d4339c8f
Giira <euan.a.marsh@gmail.com>
2025-12-02 22:25:35 +0000

    D2 P1 solve

--- day2.go
+++ day2.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"math"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type IDRange struct {
-	start uint64
-	end   uint64
+	start int
+	end   int
 }
 
 func make_ranges(instructions []string) []IDRange {
 	ranges := []IDRange{}
 	for _, instruction := range instructions {
 		pair := strings.Split(instruction, "-")
-		startID, err := strconv.ParseUint(pair[0], 10, 64)
+		startID, err := strconv.Atoi(pair[0])
 		if err != nil {
-			log.Fatalf("Error converting string to uint64: %v", err)
+			log.Fatalf("Error converting start string to int: %v", err)
 		}
-		finishID, err := strconv.ParseUint(pair[1], 10, 64)
+		finishID, err := strconv.Atoi(pair[1])
 		if err != nil {
-			log.Fatalf("Error converting string to uint64: %v", err)
+			log.Fatalf("Error converting end string to int: %v", err)
 		}
 		ranges = append(ranges, IDRange{
 			start: startID,
@@ -32,14 +33,20 @@ func make_ranges(instructions []string) []IDRange {
 	return ranges
 }
 
-func digit_tally(digit uint64) uint {
-	if digit == 0 {
-		return 1
-	} else {
-		float_digs := math.Floor(math.Log10(float64(digit))) + 1
-		return uint(float_digs)
+func part1(IDrange IDRange, result *uint64, wg *sync.WaitGroup, mu *sync.Mutex) {
+	for current := (IDrange.start); current <= IDrange.end; current++ {
+		current_str := strconv.Itoa(current)
+		if len(current_str)%2 == 0 {
+			half1 := current_str[0:(len(current_str) / 2)]
+			half2 := current_str[(len(current_str) / 2):]
+			if half1 == half2 {
+				mu.Lock()
+				*result += uint64(current)
+				mu.Unlock()
+			}
+		}
 	}
-
+	defer wg.Done()
 }
 
 func day2() {
@@ -47,4 +54,14 @@ func day2() {
 	instructions := strings.Split(data[0], ",")
 	ranges := make_ranges(instructions)
 
+	var result1 uint64 = 0
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	for _, IDRange := range ranges {
+		wg.Add(1)
+		go part1(IDRange, &result1, &wg, &mu)
+	}
+	wg.Wait()
+	fmt.Printf("Day 2, Part 1: %v\n", result1)
 }
