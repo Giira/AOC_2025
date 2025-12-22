49c2c42e1399312691c6ac9027410215e0192861
Giira <euan.a.marsh@gmail.com>
2025-12-22 21:38:17 +0000

    D8 P2 solve - soooo much easier than p1

--- day8.go
+++ day8.go
@@ -70,6 +70,39 @@ func makeCircuits(distances []Connection, CONNECTIONS int, length int) []*set.Se
 	return circuits
 }
 
+func makeCircuitsForever(distances []Connection, CONNECTIONS int, length int) Connection {
+	var circuits []*set.Set
+	for i := range length {
+		s := set.NewSet()
+		s.Add(i)
+		circuits = append(circuits, s)
+	}
+	var out Connection
+
+	for _, distance := range distances {
+		if len(circuits) == 1 {
+			break
+		}
+		var a int
+		var b int
+		for k, circuit := range circuits {
+			if circuit.Contains(distance.a) {
+				a = k
+			}
+			if circuit.Contains(distance.b) {
+				b = k
+			}
+		}
+		if a == b {
+			continue
+		}
+		circuits[a] = circuits[a].Union(circuits[b])
+		circuits = slices.Delete(circuits, b, b+1)
+		out = distance
+	}
+	return out
+}
+
 func part1_8(circuits []*set.Set) int {
 	sizes := getSizes(circuits)
 	sort.Slice(sizes, func(i, j int) bool { return sizes[i] > sizes[j] })
@@ -98,6 +131,13 @@ func getSizes(sets []*set.Set) []int {
 	return sizes
 }
 
+func part2_8(distance Connection, coords []Coord3D) {
+	x1 := coords[distance.a].x
+	x2 := coords[distance.b].x
+	out := x1 * x2
+	fmt.Printf("Day 8, Part 2: %v\n", out)
+}
+
 func day8() {
 	data := import_file("day8.txt")
 	coords := lines_to_3d_coords(data)
@@ -105,4 +145,6 @@ func day8() {
 	circuits := makeCircuits(distances, CONNECTIONS, len(coords))
 	p1 := part1_8(circuits)
 	fmt.Printf("Day 8, Part 1: %v\n", p1)
+	d := makeCircuitsForever(distances, CONNECTIONS, len(coords))
+	part2_8(d, coords)
 }
