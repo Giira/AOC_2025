4b344a0422920f7b1839c1d05f610316af88bf84
Giira <euan.a.marsh@gmail.com>
2025-12-28 10:36:58 +0000

    D9 P1 solve

--- day9.go
+++ day9.go
@@ -1,16 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
+type Rectangle struct {
+	corner1 Coord
+	corner2 Coord
+	area    int
+}
+
 func makeRectangle(coord1 Coord, coord2 Coord) int {
 	x := coord1.x - coord2.x
 	x = max(x, -x)
+	x++
 	y := coord1.y - coord2.y
 	y = max(y, -y)
+	y++
 	return x * y
 }
 
-func biggestRectangle()
+func sortRectangles(rec1 Rectangle, rec2 Rectangle) int {
+	if rec1.area < rec2.area {
+		return 1
+	} else if rec1.area > rec2.area {
+		return -1
+	} else {
+		return 0
+	}
+}
+
+func biggestRectangle(coords []Coord) []Rectangle {
+	var rectangles []Rectangle
+	for i := 0; i < len(coords); i++ {
+		for j := i + 1; j < len(coords); j++ {
+			a := makeRectangle(coords[i], coords[j])
+			rec := Rectangle{
+				corner1: coords[i],
+				corner2: coords[j],
+				area:    a,
+			}
+			rectangles = append(rectangles, rec)
+		}
+	}
+	slices.SortFunc(rectangles, sortRectangles)
+	return rectangles
+}
 
 func day9() {
 	data := import_file("day9.txt")
 	coords := lines_to_coords(data)
+	rectangles := biggestRectangle(coords)
+	fmt.Printf("Day 9, Part 1: %v\n", rectangles[0].area)
 }
